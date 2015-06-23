43af58fa15155bf9cd74e67c3939eb275dbbe179
Antoine PAUCHET <antoine.pauchet@gmail.com>
2015-06-23 10:12:07 +0200

    Fix un/register button
    passage d'un foreach a un for

--- account.php
+++ account.php
@@ -19,32 +19,39 @@
 	//if(isset($_POST["retrieve"])){
 		echo "<br>Voici la liste de vos équipement :<br>";
 		$systems = json_decode(retrieveSystems($_SESSION["username"], $_SESSION["password"]));
-		var_dump($systems);
 		
-		foreach ($systems as $obj){
+		//var_dump($systems);
+		echo "<table>";
+		for($i = 0; $i < count($systems); $i++){
+			echo "<tr>";
+			echo "<td>Equipement numero : ".$i."</td><td>Dernier ip connu : ".$systems[$i]->last_ip."</td><td>";
+		// foreach ($systems as $obj){
 			//var_dump($obj);
-			if($obj->is_register){
-				systems_button("Unregister");
-				if(isset($_POST["Unregister"])){
-					updateSystemRegistration($obj->id, 0);
-					unset($_POST["Unregister"]);
-					unset($_POST["Register"]);
+			if($systems[$i]->is_register){
+				systems_button("Unregister".$i);
+				if(isset($_POST["Unregister".$i])){
+					updateSystemRegistration($systems[$i]->id, 0);
+					unset($_POST["Unregister".$i]);
+					unset($_POST["Register".$i]);
 					header("Location: http://localhost/coSync/account.php");
 					//echo "<script language=\"javascript\" type=\"text/javascript\">window.location.reload();</script>";
 					
 					
 				}
 			}else{
-				systems_button("Register");
-				if(isset($_POST["Register"])){
-					updateSystemRegistration($obj->id, 1);
-					unset($_POST["Register"]);
-					unset($_POST["Unregister"]);
+				systems_button("Register".$i);
+				if(isset($_POST["Register".$i])){
+					updateSystemRegistration($systems[$i]->id, 1);
+					unset($_POST["Register".$i]);
+					unset($_POST["Unregister".$i]);
 					header("Location: http://localhost/coSync/account.php");
 					//echo "<script language=\"javascript\" type=\"text/javascript\">window.location.reload();</script>";
 				}
 			}
+			echo "</td></tr>";
+		// }
 		}
+		echo "</table>";
 	//}
 	
 ?>
