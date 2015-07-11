c39e436edd6d5843be7a982c052e995bbf69b4a8
Alban Paczkowski <true>
2015-07-11 17:57:10 +0200

    Work on FileMenu: |-> Add file (not finished) |-> Delete file

---
+++ src/CoSync/Services/FilesServices.java
@@ -0,0 +1,18 @@
+package CoSync.Services;
+
+import java.io.File;
+
+/**
+ * Created by Alban on 11/07/2015.
+ */
+public class FilesServices {
+    public static void addFile(String addFile, String newFile) {
+
+    }
+
+    public static void deleteFile(File file) {
+        if(file.isFile()) {
+            file.delete();
+        }
+    }
+}
