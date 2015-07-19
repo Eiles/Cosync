6eb4c16b2b38f10e77b24c02818996e71b54f90d
Elie Raffier <elie.raffier@gmail.com>
2015-07-19 15:13:22 +0200

    Reparation push

--- src/Controllers/CoVersionized.java
+++ src/Controllers/CoVersionized.java
@@ -1,28 +1,40 @@
 package Controllers;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Alban on 18/07/2015.
  */
 public class CoVersionized {
-    protected List<String> versionizedFiles;
+    //protected List<String> versionizedFiles;
+    protected HashMap<String,Boolean> versionizedFiles;
 
-    public CoVersionized() {
-        versionizedFiles = new ArrayList<>();
+    public CoVersionized(){
+        this.versionizedFiles=new HashMap<>();
     }
 
-    public synchronized List<String> getVersionizedFiles() {
+
+
+    public synchronized HashMap getVersionizedFiles() {
         return versionizedFiles;
     }
 
-    public synchronized void setVersionizedFiles(List<String> versionizedFiles) {
-        this.versionizedFiles = versionizedFiles;
+
+    public synchronized void  addVersionizedFile(String file, Boolean state){
+        versionizedFiles.put(file,state);
     }
 
     public synchronized boolean isFileInVersionized(String file) {
-
-        return versionizedFiles.contains(file);
+        if(versionizedFiles.get(file)!=null) {
+            if (versionizedFiles.get(file) == true)
+                versionizedFiles.remove(file);
+            return true;
+        }
+        return false;
     }
 }
+
